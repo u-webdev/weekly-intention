ee543a75227ea0a5fb9875abbb1c25952f6324ec
uwebury <142432719+uwebury@users.noreply.github.com>
2025-12-25 16:15:10 +0100

    Initial Commit

---
+++ Weekly Intention/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weekly Intention
+//
+//  Created by Uwe Bury on 25.12.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weekly Intention/Weekly_IntentionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weekly_IntentionApp.swift
+//  Weekly Intention
+//
+//  Created by Uwe Bury on 25.12.25.
+//
+
+import SwiftUI
+
+@main
+struct Weekly_IntentionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
