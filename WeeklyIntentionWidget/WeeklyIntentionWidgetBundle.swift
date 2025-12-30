5ef4cd810730a349786d4ed48fd87f852a2a279c
uwebury <142432719+uwebury@users.noreply.github.com>
2025-12-30 11:10:10 +0100

    Add widget extension scaffold

---
+++ WeeklyIntentionWidget/WeeklyIntentionWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WeeklyIntentionWidgetBundle.swift
+//  WeeklyIntentionWidget
+//
+//  Created by Uwe Bury on 30.12.25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WeeklyIntentionWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        WeeklyIntentionWidget()
+    }
+}
